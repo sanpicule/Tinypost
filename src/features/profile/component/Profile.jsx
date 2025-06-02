f7f37f5e5f9ab8a6fd032524fcc0dbeeeafe5b49
sanpicule <sannsi4444@gmail.com>
2025-06-02 19:38:22 +0900

    不要なボタンの削除

--- src/features/profile/component/Profile.jsx
+++ src/features/profile/component/Profile.jsx
@@ -151,14 +151,6 @@ const Profile = () => {
             pb: 10,
           }}
         >
-          <Button
-            onClick={() => navigate('/dashboard')}
-            variant="outlined"
-            color="inherit"
-            sx={{ borderRadius: 2 }}
-          >
-            戻る
-          </Button>
           <LoadingButton
             type="submit"
             variant="contained"
